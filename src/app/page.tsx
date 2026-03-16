021ed8c7b113b9cdb24daa9bb764bd4d3c665be3
Thum Hema <h3mashed@gmail.com>
2026-03-16 12:04:10 +0530

    added workout plan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero";
+import WorkoutGenerator from "./components/sections/WorkOut";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <Hero/>
+      <WorkoutGenerator/>
     </main>
   );
 }
